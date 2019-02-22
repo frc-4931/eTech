11cde2215ff8e1dafb84356024d05794f2025410
Damian0001 <damiankocherinfo@gmail.com>
2019-02-21 22:02:05 -0600

    Fixed small typos cause i didn't proof read

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const optionDefinitions = [
   { name: 'port', alias: 'p', type: Number, defaultValue: 80, description: 'Port on which to run the server. (Defualts to "80")' },
   { name: 'database-address', alias: 'a', type: String, defaultValue: "http://localhost", description: 'Address on which the server redirects database requests to. (Defualts to "http://localhost")' },
   { name: 'database-port', alias: 'o', type: Number, defaultValue: 5984, description: 'Port on which the server redirects database requests to. (Defualts to "5984")' },
-  { name: 'log-file-requests', alias: 'r', type: Boolean, description: 'Enable logging requests to for files.' },
+  { name: 'log-file-requests', alias: 'r', type: Boolean, description: 'Enable logging requests to the server for files.' },
   { name: 'log-db-requests', alias: 'l', type: Boolean, description: 'Enable logging requests for the database.' },
   { name: 'help', alias: 'h', type: Boolean, description: 'Display this usage guide.' }
 ]
