300dcba391c137fad4ba58f1aaedaa61f1bde590
Damian0001 <damiankocherinfo@gmail.com>
2019-02-21 21:30:56 -0600

    Added launch options and --help

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
-var http = require('http');
-var httpProxy = require('http-proxy');
-var fs = require("fs");
+const http = require('http');
+const httpProxy = require('http-proxy');
+const fs = require("fs");
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+const chalk = require('chalk')
 
-var LOCAL_DATABASE = "/database";
-var PROXY_TARGET = 'http://localhost:5984';
-var DIRECTORY = process.argv[2] || "";
-var PORT = parseInt(process.argv[3]) || 80;
+const optionDefinitions = [
+  { name: 'directory', alias: 'd', type: String, defaultValue: 'dist', description: 'The location of the directory to stream content from. (Defualts to "dist")' },
+  { name: 'port', alias: 'p', type: Number, defaultValue: 80, description: 'Port on which to run the server. (Defualts to "80")' },
+  { name: 'database-address', alias: 'a', type: String, defaultValue: "http://localhost", description: 'Address on which the server redirects database requests to. (Defualts to "http://localhost")' },
+  { name: 'database-port', alias: 'o', type: Number, defaultValue: 5984, description: 'Port on which the server redirects database requests to. (Defualts to "5984")' },
+  { name: 'log-file-requests', alias: 'r', type: Boolean, description: 'Enable logging requests to for files.' },
+  { name: 'log-db-requests', alias: 'l', type: Boolean, description: 'Enable logging requests for the database.' },
+  { name: 'help', alias: 'h', type: Boolean, description: 'Display this usage guide.' }
+]
 
-var proxy = httpProxy.createProxyServer({});
-var server = http.createServer(function (request, response) {
+const options = commandLineArgs(optionDefinitions, { camelCase: true, partial: true });
 
+if (options.help) {
+  const usage = commandLineUsage([
+    {
+      header: 'Scouting App - Server',
+      content: 'A web app made with Vue and PouchDB specifically for FRC scouting.'
+    },
+    {
+      header: 'Launch options',
+      optionList: optionDefinitions
+    },
+    {
+      content: 'Project home: {underline https://github.com/Damian0001/Scouting-App}'
+    }
+  ])
+  console.log(usage)
+
+  return;
+}
+
+const LOCAL_DATABASE = "/database";
+const PROXY_TARGET = options.databaseAddress + ':' + options.databasePort;
+const DIRECTORY = options.directory;
+const PORT = options.port;
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer(function (request, response) {
   var url = request.url;
 
   if (url.startsWith(LOCAL_DATABASE)) {
     var modifiedRequest = request;
     modifiedRequest.url = request.url.replace(LOCAL_DATABASE, "");
+
+    if (options.logDbRequests)
+      console.log(chalk.gray(request.connection.remoteAddress + ": " + modifiedRequest.url));
 
     proxy.web(modifiedRequest, response, {
       target: PROXY_TARGET
@@ -39,11 +76,13 @@ var server = http.createServer(function (request, response) {
       file = "/index.html";
     }
 
-    console.log(request.connection.remoteAddress + ": " + url + " > " + file);
+    if (options.logFileRequests)
+      console.log(chalk.gray(request.connection.remoteAddress + ": " + url + " > " + file));
 
     fs.readFile(DIRECTORY + file, (err, content) => {
       if (err) {
-        console.log('We cannot open file.')
+        if (options.logFileRequests)
+          console.log('We cannot open file.');
       }
 
       response.writeHead(200, {
@@ -55,5 +94,13 @@ var server = http.createServer(function (request, response) {
   }
 });
 
-console.log('Listening on port %s', PORT);
+proxy.on('error', function (err, req, response) {
+  response.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+
+  response.end('Something went wrong. Maybe the database is down?');
+});
+
+console.log(chalk.cyan('Listening on port %s\nFiles are streamed from %s\nDatabase requests are to directed to %s'), chalk.yellowBright(PORT), chalk.yellowBright(DIRECTORY), chalk.yellowBright(PROXY_TARGET));
 server.listen(PORT);
