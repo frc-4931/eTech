01a8e3c4c66f84a706e1ef80d8a2f7fedb080dde
Damian0001 <damiankocherinfo@gmail.com>
2019-03-13 20:39:36 -0500

    And added everything instead of just using -a oops

---
+++ src/components/schedule/MenuSchedule.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <Error v-if="user.role == null">You must be logged in as an admin to view this page!</Error>
+    <div
+      v-else
+      class="grid"
+    >
+      <div class="location-centered-small done-button-container">
+        <h3
+          @click="goBack()"
+          class="background-box background-box-hover content-centered"
+        >Back</h3>
+      </div>
+
+      <div class="location-span">
+        <h1 class="background-box content-centered">Schedule</h1>
+
+        <div class="background-box ranking-team-container mobile-shrink schedule-match-description">
+          <h3>Match</h3>
+          <h3>Blue</h3>
+          <h3>Red</h3>
+          <h3>Score</h3>
+          <h3>Time</h3>
+        </div>
+
+        <transition-group>
+          <ScheduleMatch
+            v-for="match in matches"
+            :key="match.match_number"
+            :matchData="match"
+          />
+        </transition-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Error from "../Error.vue";
+import ScheduleMatch from "./ScheduleMatch.vue";
+
+export default {
+  name: "MenuRanking",
+  components: {
+    ScheduleMatch,
+    Error
+  },
+  data: function() {
+    return {
+      matches: []
+    };
+  },
+  props: {
+    localtbadb: Object,
+    sync_change: Object,
+    user: Object
+  },
+  methods: {
+    reloadMatches() {
+      var dThis = this;
+
+      this.localtbadb.get("MATCHES").then(function(doc) {
+        dThis.matches = doc.json;
+
+        console.log(dThis.matches);
+      });
+    },
+    goBack() {
+      this.$router.push("/");
+    }
+  },
+  created: function() {
+    this.reloadMatches();
+
+    this.sync_change.onBlueAllianceDbChange = function(change) {
+      if (change["id"] === "MATCHES") {
+        this.reloadMatches();
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.schedule-match-description {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  text-align: center;
+}
+</style>
---
+++ src/components/schedule/ScheduleMatch.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="background-box mobile-shrink schedule-match">
+    <p>{{ matchData.match_number}}</p>
+    <p class="schedule-match-blue">{{ matchData.alliances.blue.team_keys[0] }}</p>
+    <p class="schedule-match-blue">{{ matchData.alliances.blue.team_keys[1] }}</p>
+    <p class="schedule-match-blue">{{ matchData.alliances.blue.team_keys[2] }}</p>
+    <p class="schedule-match-red">{{ matchData.alliances.red.team_keys[0] }}</p>
+    <p class="schedule-match-red">{{ matchData.alliances.red.team_keys[1] }}</p>
+    <p class="schedule-match-red">{{ matchData.alliances.red.team_keys[2] }}</p>
+    <p>{{ matchData.score_breakdown.blue.totalPoints }}</p>
+    <p>{{ matchData.score_breakdown.red.totalPoints }}</p>
+    <p>{{ scheduledTime }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RankingTeam",
+  props: {
+    matchData: Object,
+    localtbadb: Object,
+    localdb: Object
+  },
+  computed: {
+    scheduledTime: function() {
+      return new Date(this.matchData.time).toLocaleTimeString();
+    }
+  }
+};
+</script>
+
+<style>
+.schedule-match {
+  display: grid;
+  grid-template-columns: 3fr repeat(6, 1fr) repeat(2, 1.5fr) 3fr;
+  text-align: center;
+}
+.schedule-match-blue {
+  background-color: blue;
+}
+.schedule-match-red {
+  background-color: red;
+}
+</style>
