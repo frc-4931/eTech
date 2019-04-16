4dc83e658cd52c990441600935c141058fda7034
Damian0001 <damiankocherinfo@gmail.com>
2019-04-16 17:35:29 -0500

    Added version to about menu

--- src/components/credits/MenuCredits.vue
+++ src/components/credits/MenuCredits.vue
@@ -2,168 +2,78 @@
   <div class="grid">
     <div class="location-centered-small">
       <h1 class="background-box content-centered">About</h1>
-      <p class="background-box content-justified">
-        eTech is an all-in-one web application that allows users to continue
-        scouting when offline, and when the user revives their connection the
-        information is automatically synced to all other devices, and their
-        screens are updated in real time.
-      </p>
       <div class="background-box">
         <p class="content-justified">
-          If you would like to contribute to eTech or get in contact with us
-          regarding anything, you can over at our
-          <a href="https://github.com/frc-4931/eTech" target="_blank"
-            >Github repository</a
-          >.
+          eTech is an all-in-one web application that allows users to continue
+          scouting when offline, and when the user revives their connection the
+          information is automatically synced to all other devices, and their
+          screens are updated in real time.
         </p>
+
+        <p class="content-right">Running version: {{ version }}</p>
       </div>
+
+      <p class="content-justified background-box">
+        If you would like to contribute to eTech or get in contact with us
+        regarding anything, you can at our
+        <a href="https://github.com/frc-4931/eTech" target="_blank"
+          >Github repository</a
+        >.
+      </p>
     </div>
 
-    <div class="location-centered-small credits-container">
-      <h1 class="location-centered-small background-box content-centered">
+    <div class="location-centered-small small-margin">
+      <h1 class="background-box content-centered">
         Credits
       </h1>
 
-      <div class="grid-perminant">
-        <div class="location-left">
-          <p class="background-box">Programming</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">James Crane</p>
-          <p class="background-box">Damian Kocher</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">User Interface</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">Damian Kocher</p>
-        </div>
+      <div v-for="(credit) in credits" :key="credit" class="grid-perminant small-margin-sub">
+        <p class="background-box location-left">{{ credit[0]}}</p>
+        <p v-for="recipient in credit[1]" :key="recipient" class="background-box location-right content-right">{{ recipient }}</p>
       </div>
     </div>
 
-    <div class="location-centered-small credits-container">
-      <h1 class="location-centered-small background-box content-centered">
+    <div class="location-centered-small small-margin">
+      <h1 class="background-box content-centered">
         Licenses
       </h1>
 
-      <div class="grid-perminant">
-        <div class="location-left">
-          <p class="background-box">Vue</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Vue-CLI</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Vue-Router</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Vue-Scrollto</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">PouchDB</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">Apache License 2.0</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">PouchDB-Authentication</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">Apache License 2.0</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Chalk</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Command-Line-Args</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Command-Line-Usage</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">HTTP</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">BSD-2-Clause</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">HTTP-Proxy</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">HTTPS</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">ISC</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Lodash</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">MIT</p>
-        </div>
-
-        <div class="location-left">
-          <p class="background-box">Request</p>
-        </div>
-        <div class="location-right">
-          <p class="background-box">Apache License 2.0</p>
-        </div>
+      <div v-for="license in licenses" :key="license" class="grid-perminant small-margin-sub">
+        <p class="background-box location-left">{{ license[0]}}</p>
+        <p class="background-box location-right content-right">{{ license[1]}}</p>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MenuCredits"
+  name: "MenuCredits",
+  data() {
+    return {
+      version: JSON.parse(
+        unescape(process.env.PACKAGE_JSON || "%7Bversion%3A0%7D")
+      ).version,
+      credits: [
+        ["Programming", ["James Crane", "Damian Kocher"]],
+        ["User Interface", ["Damian Kocher"]]
+      ],
+      licenses: [
+        ["Vue", "MIT"],
+        ["Vue-CLI", "MIT"],
+        ["Vue-Router", "MIT"],
+        ["Vue-Scrollto", "MIT"],
+        ["PouchDB", "Apache License 2.0"],
+        ["PouchDB-Authentication", "Apache License 2.0"],
+        ["Chalk", "MIT"],
+        ["Command-Line-Args", "MIT"],
+        ["Command-Line-Usage", "MIT"],
+        ["HTTP", "BSD-2-Clause"],
+        ["HTTP-Proxy", "MIT"],
+        ["HTTPS", "ISC"],
+        ["Lodash", "MIT"],
+        ["Request", "Apache License 2.0"]
+      ]
+    };
+  }
 };
 </script>
-
-<style>
-.credits-container .grid-perminant div p {
-  margin-top: 0px;
-}
-.credits-container .grid-perminant div:nth-child(2n + 1) .background-box {
-  margin-right: 5px;
-}
-.credits-container .grid-perminant div:nth-child(2n) .background-box {
-  margin-left: 5px;
-}
-</style>
